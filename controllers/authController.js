5f76257527843723046261aaca644829f9c93ba5
bluuweb <contacto@bluuweb.cl>
2022-03-09 15:50:04 -0300

    arreglo ancla email

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,7 @@ const registerUser = async (req, res) => {
             subject: "Verifica tu cuenta de correo", // Subject line
             html: `<a href="${
                 process.env.PATHHEROKU || "http://localhost:5000"
-            }/auth/confirmar/${user.tokenConfirm}">Verifica tu cuenta aquí</a>`, // html body
+            }auth/confirmar/${user.tokenConfirm}">Verifica tu cuenta aquí</a>`, // html body
         });
 
         req.flash("mensajes", [
